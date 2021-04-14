07c4594af6546026a3d1fa37788de932fba1d070
Shalini Mittal <Shalini@Shalinis-MacBook-Pro.local>
2021-04-14 10:39:25 +0530

    second commit

---
+++ TGMavenDemo/src/main/java/com/techgatha/App1.java
@@ -0,0 +1,14 @@
+package com.techgatha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Yeahhhh executing from Jenkins......" );
+    }
+}
